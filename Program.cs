b021419de95f3413a01e84a8400f03e8c5efdf06
Leonardo168 <leonardo-96@hotmail.com.br>
2025-09-22 15:46:56 -0300

    Classe Iphone agora herda da classe Smartphone, método InstalarAplicativo sobrescrito

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
 System.Console.WriteLine("\nSmartphone Nokia:");
 Smartphone nokia = new Nokia(numero: "(99) 9999-9999", modelo: "G42", imei: "111111111111111", memoria: 128);
 nokia.Ligar();
 nokia.ReceberLigacao();
 nokia.InstalarAplicativo("Whatsapp");
+
+System.Console.WriteLine("\nSmartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "(88) 8888-8888", modelo: "iPhone 17 Pro Max", imei: "222222222222222", memoria: 512);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
