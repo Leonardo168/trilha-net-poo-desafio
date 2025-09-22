416ab28cb86a129f7b3ca791f47f7240716bb178
Leonardo168 <leonardo-96@hotmail.com.br>
2025-09-22 15:34:48 -0300

    Classe Nokia agora herda da classe Smartphone, método InstalarAplicativo sobrescrito

--- Program.cs
+++ Program.cs
@@ -1,3 +1,8 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+System.Console.WriteLine("\nSmartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "(99) 9999-9999", modelo: "G42", imei: "111111111111111", memoria: 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
